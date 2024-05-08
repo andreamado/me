06d411a31be99f52eedd1e2512f927964ba485e2
André Amado <andre.amado@pm.me>
2024-05-08 15:50:13 +0100

    added the contactme page

--- compile.py
+++ compile.py
@@ -2,3 +2,4 @@ from os import system
 
 system('mustache data.json science.mustache.html > science.html')
 system('mustache data.json work_studies.mustache.html > work_studies.html')
+system('mustache data.json contactme.mustache.html > contactme.html')
